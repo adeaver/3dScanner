f92a9e01d0da5f8acc6974feee7a40f77288fe5a
adeaver <deaver.andrew@gmail.com>
2016-09-17 16:05:43 -0400

    Interface with panning servo

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -1,9 +1,17 @@
+#include <Servo.h> 
+ 
+Servo pan;
+
 int sensorPin = A0;
+int servoPinPan = 9;
+
+int pos = 0;
 
 float sensorOutput;
 
 void setup() {
   pinMode(sensorPin, INPUT);
+  pan.attach(servoPinPan);
   Serial.begin(9600);
 }
 
@@ -12,6 +20,18 @@ double calcDistance(float voltage) {
 }
 
 void loop() {
-  sensorOutput = (analogRead(sensorPin)/1023.0)*5.0;
-  Serial.println(calcDistance(sensorOutput));
+  for(pos = 45; pos <= 135; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    pan.write(pos);              // tell servo to go to position in variable 'pos' 
+    sensorOutput = (analogRead(sensorPin)/1023.0)*5.0;
+    Serial.println(calcDistance(sensorOutput));
+    delay(100);                       // waits 15ms for the servo to reach the position 
+  } 
+    for(pos = 135; pos >= 45; pos -= 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    pan.write(pos);              // tell servo to go to position in variable 'pos' 
+    sensorOutput = (analogRead(sensorPin)/1023.0)*5.0;
+    Serial.println(calcDistance(sensorOutput));
+    delay(100);                       // waits 15ms for the servo to reach the position 
+  } 
 }
