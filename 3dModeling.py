cf6327c0c363fa80f59c09a07c798a0d6a86b0e2
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-09-22 23:11:11 -0400

    push the 3d modelling code

--- 3dModeling.py
+++ 3dModeling.py
@@ -4,35 +4,60 @@ import matplotlib.pyplot as plt
 
 startFlag = False
 finishCycle = False
+initialize = False
+
+
+horizontalList = []
+verticalList = []
 
 with serial.Serial('/dev/cu.usbmodem1421', 9600, timeout=1) as ser:
-     while not finishCycle:
+	while not initialize:
 		line = ser.readline()
 		print line
-		if line.strip() == "START":
+		if line.strip() == "BEGIN_SCAN":
+			initialize = True
+
+	while not finishCycle:
+		line = ser.readline()
+		print line
+		if line.strip() == "START_LINE":
 			startFlag = True
-			xyList = []
+			horizontalList = []
 			continue
-		if line.strip() == "END":
+		if line.strip() == "END_LINE":
 			if startFlag == True:
-				finishCycle = True
 				startFlag = False
-				break
+				verticalList.append(horizontalList)
+				horizontalList = []
+		if line.strip() == "FINISH":
+			finishCycle = True
 		if startFlag:
 			fLine = float(line)
-			xyList.append(fLine)
-
+			horizontalList.append(fLine)
+print "horizontal: ", len(verticalList[0])
+print "vertical: ", len(verticalList)
 print "out of the loop"
-angleList = range(65, 116)
-xList = []
-yList = []
-for index in range(len(xyList)):
-	dst = xyList[index]
-	xList.append(dst*math.cos(angleList[index]*math.pi/180))
-	yList.append(dst*math.sin(angleList[index]*math.pi/180))
 
+def dgreeToRadian(degreeAngle):
+	return degreeAngle*math.pi/180
 
-plt.plot(xList, yList, 'ro')
+def getXYZ(dList, phi):
+	angleList = range(65, 116)
+	xList = []
+	yList = []
+	zList = []
+
+	for index in range(len(dList)):
+		d = dList[index]
+		dxy = d*math.cos(dgreeToRadian(phi))
+		xList.append(dxy*math.cos(dgreeToRadian(angleList[index])))
+		yList.append(dxy*math.sin(dgreeToRadian(angleList[index])))
+		zList.append(d*math.sin(dgreeToRadian(phi)))
+	return xList, yList, zList
+
+for index in range(len(verticalList)):
+	xList, yList, zList = getXYZ(verticalList[index], index)
+	plt.plot(xList, yList, 'ro')
 plt.show()
 
 
