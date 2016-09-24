d492ba4b8997438dc644e90e58e9cc57d2db6dac
adeaver <deaver.andrew@gmail.com>
2016-09-24 16:35:41 -0400

    New scanner code

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -1,40 +1,68 @@
 #include <Servo.h> 
  
 Servo pan;
+Servo tilt;
 
 int sensorPin = A0;
 int servoPinPan = 9;
+int servoPinTilt = 10;
 
-int pos = 0;
+// Position variables
+int h_pos = 0;
+int v_pos = 0;
+
+// min-max value for the angles
+int min_h = 65;
+int max_h = 115;
+
+int min_v = 10;
+int max_v = 30;
 
 float sensorOutput;
 
 void setup() {
   pinMode(sensorPin, INPUT);
   pan.attach(servoPinPan);
+  tilt.attach(servoPinTilt);
   Serial.begin(9600);
 }
 
+// calculates the distance given a voltage
 double calcDistance(float voltage) {
  return -13.582 * pow(voltage, 3) + 87.266 * pow(voltage, 2) - 201.117 * voltage + 192.102; 
 }
 
-void loop() {
-  Serial.println("START");
-  for(pos = 65; pos <= 115; pos += 1) // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    pan.write(pos);              // tell servo to go to position in variable 'pos' 
+// scans a line
+void scanLine() {
+  // Print Token to Begin Collecting line data
+  Serial.println("START_LINE");
+  for(h_pos = min_h; h_pos <= max_h; h_pos += 1) {
+    
+    // Write panning servo to a given position    
+    pan.write(h_pos);
+    
+    // This should STOP the arduino to allow the servo to catch up
+    // Debouncing would allow the loop to continue    
+    delay(100);
+        
+    // Read the sensor value when the servo is at a given position
     sensorOutput = (analogRead(sensorPin)/1023.0)*5.0;
-    Serial.println(calcDistance(sensorOutput));
-    delay(100);                       // waits 15ms for the servo to reach the position 
-  } 
-  Serial.println("END");
-//    for(pos = 135; pos >= 45; pos -= 1) // goes from 0 degrees to 180 degrees 
-//  {                                  // in steps of 1 degree 
-//    pan.write(pos);              // tell servo to go to position in variable 'pos' 
-//    sensorOutput = (analogRead(sensorPin)/1023.0)*5.0;
-//    Serial.println(calcDistance(sensorOutput));
-//    delay(100);                       // waits 15ms for the servo to reach the position 
-//  }
+    Serial.println(calcDistance(sensorOutput)); 
+  }
+  // Print token to End collecting line data 
+  Serial.println("END_LINE");  
+}
+
+void loop() {
+  // Started Scanning Token
+  Serial.println("BEGIN_SCAN");
+
+  // Move tilt servo
+  for(v_pos = max_v; v_pos >= min_v; v_pos -= 5) {
+    tilt.write(v_pos);
+    scanLine(); 
+  }
   
+  // Done Scanning Token
+  Serial.println("END_SCAN");  
 }
